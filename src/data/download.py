b8ecdee30b79539b0074c2c329146911032b13bb
MarkusFuchs68 <markus_fuchs@gmx.net>
2025-04-30 11:13:38 +0200

    refactored data loading

--- src/data/download.py
+++ src/data/download.py
@@ -15,7 +15,7 @@ def download_data(url, save_path):
 
 if __name__ == "__main__":
     # Define the path to save the data
-    data_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'raw_data')
+    data_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'raw')
     print(data_dir)
     os.makedirs(data_dir, exist_ok=True)
     save_path = os.path.join(data_dir, 'raw.csv')
